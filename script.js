fbcd0913f970bdfd37443567d3636a14996fc263
Alexandre Halas <alexandrehalas@gmail.com>
2023-10-23 14:29:27 -0300

    CommonJS Modules

--- script.js
+++ script.js
@@ -71,3 +71,18 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+
+// COMMONJS MODULES
+
+/*
+/ Export
+export.addTocart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
+*/
